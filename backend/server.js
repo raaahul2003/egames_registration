d9a577b306950de8638ce80546e69fdf705cbfda
Rahul Raj C P <rahulrajcp@Mac.lan>
2026-03-09 00:07:18 +0530

    Final fix

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 // ── CORS ────────────────────────────────────────────────────
-const cors = require("cors");
+
 app.use(cors());
 
 // app.use(cors({
